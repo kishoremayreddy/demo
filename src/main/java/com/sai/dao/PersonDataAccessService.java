d4ebc0f175d3cebd789b83ce10046555698e19b3
kishoremayreddy <kishoremayreddy@gmail.com>
2020-04-19 13:18:39 -0500

    initial commit

---
+++ src/main/java/com/sai/dao/PersonDataAccessService.java
@@ -0,0 +1,51 @@
+package com.sai.dao;
+
+import com.sai.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository("postgres")
+public class PersonDataAccessService implements PersonDao {
+
+    private final JdbcTemplate jdbcTemplate;
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        final String sql = "SELECT id, name FROM PERSON";
+       return jdbcTemplate.query(sql, (resultSet,i)->{
+            return new Person(UUID.fromString(resultSet.getString("id")),
+                    resultSet.getString("name"));
+        });
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
+    }
+}
